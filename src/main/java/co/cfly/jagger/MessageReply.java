25146872031893e1db6fb567eabf38aacf23063f
Ken Mix <kmix@kmix.net>
2011-10-07 16:01:29 -0600

    - More verbose L2 debugs

--- src/main/java/co/cfly/jagger/MessageReply.java
+++ src/main/java/co/cfly/jagger/MessageReply.java
@@ -83,6 +83,7 @@ public class MessageReply implements Runnable
                     reply.setTo(entry.getUser());
                     reply.setBody(returnMessage);
                     xmppConn.sendPacket(reply);
+                    JaggerAgent.doDebug(2, "Sent to " + replyAddr + ": " + returnMessage);
                 }
             }
         }
@@ -93,6 +94,7 @@ public class MessageReply implements Runnable
             reply.setTo(replyAddr);
             reply.setBody(returnMessage);
             xmppConn.sendPacket(reply);
+            JaggerAgent.doDebug(2, "Sent single response to " + replyAddr + ": " + returnMessage);
         }
     }
 }
